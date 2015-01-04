9fedfff07deccc80cc3a0dec89a9c8dbcf8a5b12
Sybren Gjaltema <lolmewn@gmail.com>
2015-01-04 23:01:57 +0100

    Make StatsStatHolder thread-safe

--- src/main/java/nl/lolmewn/stats/user/StatsStatHolder.java
+++ src/main/java/nl/lolmewn/stats/user/StatsStatHolder.java
@@ -1,17 +1,29 @@
 package nl.lolmewn.stats.user;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import nl.lolmewn.stats.api.stat.Stat;
+import nl.lolmewn.stats.api.stat.StatEntry;
+import nl.lolmewn.stats.api.user.StatsHolder;
 
 /**
  *
  * @author Lolmewn
  */
-public class StatsStatHolder extends DefaultStatsHolder {
+public class StatsStatHolder implements StatsHolder {
 
+    private final UUID uuid;
     private boolean temp = true;
-    
+    private final Map<Stat, List<StatEntry>> entries;
+
     public StatsStatHolder(UUID uuid) {
-        super(uuid);
+        this.uuid = uuid;
+        this.entries = new ConcurrentHashMap<>();
     }
 
     public boolean isTemp() {
@@ -20,6 +32,34 @@ public class StatsStatHolder extends DefaultStatsHolder {
 
     public void setTemp(boolean value) {
         this.temp = value;
+    }
+
+    @Override
+    public void addEntry(Stat stat, StatEntry entry) {
+        if (!this.hasStat(stat)) {
+            this.entries.put(stat, Collections.synchronizedList(new LinkedList<StatEntry>()));
+        }
+        this.entries.get(stat).add(entry);
+    }
+
+    @Override
+    public Collection<Stat> getStats() {
+        return entries.keySet();
+    }
+
+    @Override
+    public Collection<StatEntry> getStats(Stat stat) {
+        return entries.get(stat);
+    }
+
+    @Override
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public boolean hasStat(Stat stat) {
+        return entries.containsKey(stat);
     }
 
 }
