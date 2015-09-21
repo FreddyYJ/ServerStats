76fe8fb31ef251d45e16e228db0b4c14d7d9b2c7
Sybren Gjaltema <lolmewn@gmail.com>
2015-09-21 10:10:48 +0200

    Exceptions are so... breaking.

--- src/main/java/nl/lolmewn/stats/util/Timings.java
+++ src/main/java/nl/lolmewn/stats/util/Timings.java
@@ -9,26 +9,20 @@ import java.util.HashMap;
 public class Timings {
 
     private static boolean enabled = false;
-    private final static HashMap<String, Long> timings = new HashMap<>();
+    private final static HashMap<String, Long> TIMINGS = new HashMap<>();
 
     public static void startTiming(String name, long nanos) {
         if (!enabled) {
             return;
         }
-        if (timings.containsKey(name)) {
-            throw new IllegalStateException("Timings already started for " + name);
-        }
-        timings.put(name, nanos);
+        TIMINGS.put(name, nanos);
     }
 
     public static long finishTimings(String name, long nanos) {
         if (!enabled) {
             return -1;
         }
-        if (!timings.containsKey(name)) {
-            throw new IllegalStateException("Timings were not started for " + name);
-        }
-        return nanos - timings.remove(name);
+        return nanos - TIMINGS.remove(name);
     }
 
     public static void setEnabled(boolean value) {
