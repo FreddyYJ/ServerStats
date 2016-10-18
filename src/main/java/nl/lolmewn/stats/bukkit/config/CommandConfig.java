d38d4554c8c866d69b1b4cd2ec18f168a162bddc
Sybren Gjaltema <lolmewn@gmail.com>
2016-10-18 12:46:15 +0200

    Implements the first version of the in-game configurator for /stats.

--- src/main/java/nl/lolmewn/stats/bukkit/config/CommandConfig.java
+++ src/main/java/nl/lolmewn/stats/bukkit/config/CommandConfig.java
@@ -38,6 +38,11 @@ public class CommandConfig {
     private void init() {
         List<String> statsToShow = plugin.getConfig().getStringList("statsCommand.show");
         statsToShow.forEach(this::addItemForStat);
+        mainInventory.addItem(new ManagedItem(Material.STAINED_GLASS_PANE)
+                .setItemDescription("Click to add new stat")
+                .setItemName("Add new")
+                .setOnClickAction(this::chooseStat)
+        );
     }
 
     private void addItemForStat(String desc) {
@@ -45,6 +50,7 @@ public class CommandConfig {
         if (descriptor == null) {
             return; // Well, at least I tried.
         }
+        plugin.debug("Adding item to command inv for " + descriptor.getStat() + " with conditions " + descriptor.getConditions());
         ItemStack stack = new ItemStack(Material.WOOL);
         fixItemMeta(stack, descriptor);
         ManagedItem item = new ManagedItem(stack);
@@ -62,6 +68,7 @@ public class CommandConfig {
     }
 
     private ManagedInventory createStatInventory(StatDescriptor descriptor) {
+        plugin.debug("Generating stat inventory for " + descriptor);
         ManagedInventory inv = new ManagedInventory(plugin, "Editing " + descriptor.getStat().getName());
         if (descriptor.getStat().getDataTypes() == null || descriptor.getStat().getDataTypes().isEmpty()) {
             // Well - nothing really to configure here
@@ -74,21 +81,46 @@ public class CommandConfig {
                 // There is a condition for this metadata!
                 Condition cond = condition.get();
                 inv.addItem(
-                        new ManagedItem(Material.REDSTONE_LAMP_ON)
+                        new ManagedItem(Material.REDSTONE_TORCH_ON)
                         .setItemName(metadata.getKey())
-                        .setItemDescription("Mode: " + cond.getMode(), "Matches: " + cond.getMatches())
-                        .setOnClickAction(player -> player.sendMessage("Not yet implemented")));
+                        .setItemDescription("Mode: " + cond.getMode(), "Matches: " + cond.getMatches(), "Click to edit or disable")
+                        .setOnClickAction(player -> {
+                            player.closeInventory();
+                            new CommandConversation(plugin, player, descriptor, cond.getName(), inv, this);
+                        }));
             } else {
                 inv.addItem(
                         new ManagedItem(Material.REDSTONE_LAMP_OFF)
                         .setItemName(metadata.getKey())
                         .setItemDescription("Click to enable")
-                        .setOnClickAction(player -> player.sendMessage("Not yet implemented"))
+                        .setOnClickAction(player -> {
+                            player.closeInventory();
+                            new CommandConversation(plugin, player, descriptor, metadata.getKey(), inv, this);
+                        })
                 );
             }
         });
         addBackButton(inv, mainInventory);
         return inv;
+    }
+
+    private void chooseStat(Player player) {
+        ManagedInventory inv = new ManagedInventory(plugin, "Choose a stat");
+        plugin.getStatManager().getStats().forEach(stat -> {
+            inv.addItem(new ManagedItem(Material.DIAMOND)
+                    .setItemName(stat.getName())
+                    .setItemDescription(stat.getDescription())
+                    .setOnClickAction(p -> {
+                        this.mainInventory.clear();
+                        List<String> curr = plugin.getConfig().getStringList("statsCommand.show");
+                        curr.add(stat.getName());
+                        plugin.getConfig().set("statsCommand.show", curr);
+                        plugin.saveConfig();
+                        this.init();
+                        createStatInventory(new StatDescriptor(stat)).open(p);
+                    }));
+        });
+        inv.open(player);
     }
 
     private void addBackButton(ManagedInventory inv, ManagedInventory previous) {
@@ -98,6 +130,12 @@ public class CommandConfig {
                 .setItemName("&1Go back")
                 .setItemDescription("Takes you to the previous inventory")
                 .setOnClickAction(player -> previous.open(player)));
+    }
+
+    public void reloadStat(StatDescriptor desc) {
+        ManagedInventory old = this.statInventories.remove(desc.getStat());
+        ManagedInventory created = this.createStatInventory(desc);
+        old.consume(created);
     }
 
 }
--- src/main/java/nl/lolmewn/stats/bukkit/config/CommandConversation.java
+++ src/main/java/nl/lolmewn/stats/bukkit/config/CommandConversation.java
@@ -1,11 +1,23 @@
 package nl.lolmewn.stats.bukkit.config;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import mkremins.fanciful.FancyMessage;
+import nl.lolmewn.itemmanager.inv.ManagedInventory;
+import nl.lolmewn.stats.Condition;
+import nl.lolmewn.stats.Condition.MatchingMode;
+import nl.lolmewn.stats.api.storage.DataType;
+import nl.lolmewn.stats.bukkit.BukkitMain;
+import org.bukkit.ChatColor;
+import org.bukkit.conversations.Conversation;
+import org.bukkit.conversations.ConversationAbandonedEvent;
 import org.bukkit.conversations.ConversationContext;
 import org.bukkit.conversations.ConversationFactory;
-import org.bukkit.conversations.FixedSetPrompt;
+import org.bukkit.conversations.MessagePrompt;
 import org.bukkit.conversations.Prompt;
+import org.bukkit.conversations.StringPrompt;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 /**
  * This class starts a conversation with a Player for setting up metadata for a
@@ -15,27 +27,139 @@ import org.bukkit.plugin.Plugin;
  */
 public class CommandConversation {
 
+    private final BukkitMain plugin;
     private final StatDescriptor desc;
+    private final CommandConfig config;
+    private final String condName;
+    private final Player player;
+    private final ManagedInventory inventory;
 
-    public CommandConversation(Plugin plugin, Player player, StatDescriptor descriptor) {
+    public CommandConversation(
+            BukkitMain plugin,
+            Player player,
+            StatDescriptor descriptor,
+            String condName,
+            ManagedInventory previous,
+            CommandConfig config) {
+        this.plugin = plugin;
+        this.player = player;
+        this.config = config;
         this.desc = descriptor;
-        new ConversationFactory(plugin)
-                .withEscapeSequence("quit")
+        this.condName = condName;
+        this.inventory = previous;
+        Conversation conv = new ConversationFactory(plugin)
+                .withEscapeSequence("/quit")
                 .withFirstPrompt(new MainMenuPrompt())
                 .withLocalEcho(false)
-                .withModality(true).buildConversation(player);
+                .withModality(true)
+                .buildConversation(player);
+        conv.addConversationAbandonedListener((ConversationAbandonedEvent unused) -> {
+            if (player.isOnline()) {
+                inventory.open(player);
+            }
+        });
+        conv.begin();
+        plugin.debug("Opening conversation for " + player.getName() + " for stat " + desc.getStat().getName() + " and cond " + condName);
     }
 
-    private class MainMenuPrompt extends FixedSetPrompt {
-
-        @Override
-        protected Prompt acceptValidatedInput(ConversationContext context, String input) {
-            return Prompt.END_OF_CONVERSATION;
-        }
+    private class MainMenuPrompt extends MessagePrompt {
 
         @Override
         public String getPromptText(ConversationContext context) {
-            return "To-implement";
+            return ChatColor.DARK_AQUA + "You are editing the " + ChatColor.DARK_RED + condName + ChatColor.DARK_AQUA + " condition\n"
+                    + ChatColor.DARK_AQUA + "To leave this prompt, use " + ChatColor.GOLD + "/quit\n"
+                    + ChatColor.DARK_AQUA + "This condition is of type "
+                    + ChatColor.DARK_RED + desc.getStat().getDataTypes().getOrDefault(condName, DataType.STRING).toString().toLowerCase() + "\n";
+        }
+
+        @Override
+        protected Prompt getNextPrompt(ConversationContext context) {
+            return new InputPrompt();
+        }
+
+    }
+
+    private class InputPrompt extends StringPrompt {
+
+        @Override
+        public String getPromptText(ConversationContext context) {
+            new FancyMessage("Click here").color(ChatColor.GOLD).style(ChatColor.UNDERLINE).command("disable").then()
+                    .text(" to disable this condition or ").color(ChatColor.DARK_AQUA).then()
+                    .text("click here").color(ChatColor.GOLD).style(ChatColor.UNDERLINE).command("quit").then()
+                    .text(" to go back to the " + desc.getStat().getName() + " inventory").color(ChatColor.DARK_AQUA).send(player);
+            return ChatColor.DARK_AQUA + "Please input the condition line (see "
+                    + ChatColor.GOLD + ChatColor.UNDERLINE + "http://bit.ly/s3conf" + ChatColor.RESET + ChatColor.DARK_AQUA + " for help)";
+        }
+
+        @Override
+        public Prompt acceptInput(ConversationContext context, String input) {
+            if (input.equalsIgnoreCase("disable")) {
+                // Sure thing.
+                removeCondition(condName, true);
+                config.reloadStat(desc);
+                return END_OF_CONVERSATION;
+            } else if (input.equalsIgnoreCase("quit")) {
+                return END_OF_CONVERSATION;
+            }
+            // validate cond
+            Condition parsed = Condition.parse(condName + "=" + input);
+            if (parsed == null) {
+                player.sendMessage(ChatColor.RED + "Could not parse your condition! Please try again.");
+                return this;
+            } else {
+                // wowe, a new condition. Better save it!
+                addOrEditCondition(condName, parsed);
+                config.reloadStat(desc);
+                return END_OF_CONVERSATION;
+            }
+        }
+
+        private void addOrEditCondition(String oldName, Condition newer) {
+            StatDescriptor old = new StatDescriptor(desc.getStat(), desc.getConditions().toArray(new Condition[desc.getConditions().size()]));
+            removeCondition(oldName, false);
+            desc.addCondition(newer);
+            saveStatDescriptor(desc, old);
+        }
+
+        private void removeCondition(String condName, boolean save) {
+            Optional<Condition> cond = desc.getConditions().stream().filter(c -> c.getName().equals(condName)).findAny();
+            if (!cond.isPresent()) {
+                return; // Don't have to check save either since there is no change.
+            }
+            StatDescriptor old = new StatDescriptor(desc.getStat(), desc.getConditions().toArray(new Condition[desc.getConditions().size()]));
+            desc.getConditions().remove(cond.get());
+            if (save) {
+                saveStatDescriptor(desc, old);
+            }
+        }
+
+        private void saveStatDescriptor(StatDescriptor desc, StatDescriptor old) {
+            String line = generateLine(desc);
+            List<String> showList = plugin.getConfig().getStringList("statsCommand.show");
+            List<String> list = showList.stream().filter(str -> !StatDescriptor.parse(str, plugin.getStatManager()).equals(old)).collect(Collectors.toList());
+            list.add(line);
+            plugin.getConfig().set("statsCommand.show", list);
+            plugin.saveConfig();
+        }
+
+        private String generateLine(StatDescriptor desc) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(desc.getStat().getName());
+            desc.getConditions().stream().forEach((cond) -> {
+                sb.append(",").append(cond.getName()).append("=");
+                MatchingMode mode = cond.getMode();
+                cond.getMatches().stream().forEach(match -> {
+                    if (!sb.toString().endsWith("=")) {
+                        // not first
+                        sb.append("|");
+                    }
+                    if (mode == MatchingMode.EXCLUDING) {
+                        sb.append("!");
+                    }
+                    sb.append(match);
+                });
+            });
+            return sb.toString();
         }
 
     }
